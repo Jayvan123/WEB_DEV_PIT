5f95fc1d90f46c0058f7120ccc6b5bb358f514f0
Jovan <126861699+Jayvan123@users.noreply.github.com>
2024-06-05 19:31:52 +0800

    edit
    edit

--- src/COMPONENTS/APPLICATION_WINDOWS/Application2.jsx
+++ src/COMPONENTS/APPLICATION_WINDOWS/Application2.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo_img from '../../IMAGES/logo-school.png';
 
 const Application2 = () => {
   return (
     <div className='flex flex-col h-screen'> {/* Header */}
-      
       <div className='h-[90px] bg-[#072F5F] text-white flex items-center'>
         <div className='h-[90px] w-[90px] ml-[10px]'>
           <img src={logo_img} alt="" className='object-cover'/>
@@ -14,45 +15,34 @@ const Application2 = () => {
       </div>
 
       <div className='flex flex-1 h-full'> {/* Sidebar */}
-        
-        <div className='w-[80px] bg-[#213A59] text-white flex flex-col'>
-        </div>
-
-       
+        <div className='w-[80px] bg-[#213A59] text-white flex flex-col'></div>
         <div className='flex-1 p-4'>  {/* Main Content */}
           <div className='h-[40px] bg-[#D1DDED] rounded-[5px]'></div>
           <div className='h-[520px] mt-[10px] bg-[#FFFFFF] rounded-[5px] border-solid border-black border-[1px]'>
-
             <div className='flex h-[45px] w-[500px] bg-[#213A59] ml-[15px] mt-[15px] rounded-[5px] text-[20px] text-white font-serif items-center justify-center'>
               <h1>Please complete the form for your application:</h1>
             </div>
-
             <h1 className='text-[20px] font-serif ml-[15px] mt-[20px]'>PERSONAL INFORMATION</h1>
             <h1 className='text-[20px] font-serif ml-[15px]'>
               __________________________________________________________________________________________________________
             </h1>
-
             <div className='flex flex-col'>
-
               <div className='flex flex-row'>
                 <div className='flex items-center justify-between w-full'>
-                    <NavLink to='/' className='ml-[15px] mt-[190px] font-sans text-white text-[15px] font-bold bg-[#3784DC] h-[35px] w-[90px] rounded-[3px] flex items-center justify-center'>
-                        Back  
-                    </NavLink>
-
-                    <NavLink to='/' className='mr-[15px] mt-[190px] font-sans text-white text-[15px] font-bold bg-[#3784DC] h-[35px] w-[90px] rounded-[3px] flex items-center justify-center'>
-                        Proceed
-                    </NavLink>
-                  </div>
+                  <NavLink to='/application1' className='ml-[15px] mt-[190px] font-sans text-white text-[15px] font-bold bg-[#3784DC] h-[35px] w-[90px] rounded-[3px] flex items-center justify-center'>
+                    Back  
+                  </NavLink>
+                  <NavLink to='/application2' className='mr-[15px] mt-[190px] font-sans text-white text-[15px] font-bold bg-[#3784DC] h-[35px] w-[90px] rounded-[3px] flex items-center justify-center'>
+                    Proceed
+                  </NavLink>
+                </div>
               </div>
-              
             </div>
-
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Application2
+export default Application2;
