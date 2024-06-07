e791ef44ca44df712f2a77a29af6fe51fdeec718
Jovan <126861699+Jayvan123@users.noreply.github.com>
2024-06-07 20:36:36 +0800

    add
    add

--- src/COMPONENTS/DASHBOARD_WINDOWS/Header.jsx
+++ src/COMPONENTS/DASHBOARD_WINDOWS/Header.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React from 'react';
 import { NavLink } from 'react-router-dom';
-import logo_img from '../../IMAGES/logo-school.png';
+import logo_school from '../../IMAGES/logo-school.png';
+import user_logo from '../../IMAGES/IMAGES_DASHBOARD/user-logo.png';
 
 const Header = () => {
   return (
     <div className='h-[90px] bg-[#072F5F] text-white flex items-center'>
       <div className='h-[90px] w-[90px] ml-[10px]'>
-        <img src={logo_img} alt="" className='object-cover'/>
+        <img src={logo_school} alt="" className='object-cover' />
       </div>
+      
       <div className='flex ml-[20px] text-white text-[20px] font-serif'>
         JMHTech Institute: Empowering Tomorrows Innovators
       </div>
+      
+      <div className='flex items-center ml-[600px] mr-[50px]'>
+        <div className='h-[70px] w-[70px]'>
+          <img src={user_logo} alt="" className='object-cover' />
+        </div>
+        <div className='ml-[10px] flex flex-col'>
+          <label htmlFor="userlabel" className='text-white text-[20px] font-sans'>Student Name</label>
+          {/* <p className='text-white text-[15px] font-sans'>Year Level</p> */}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
