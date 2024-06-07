9ce31b9dfecee205aba9758bec6523d7e4d2fb7d
Hanzamune69 <127651010+Hanzamune69@users.noreply.github.com>
2024-06-07 21:45:40 +0800

    add
    add

--- src/COMPONENTS/DASHBOARD_WINDOWS/Enrollment.jsx
+++ src/COMPONENTS/DASHBOARD_WINDOWS/Enrollment.jsx
@@ -43,7 +43,69 @@ const Enrollment = () => {
             <h1 className='text-[20px] font-serif ml-[15px]'>
               __________________________________________________________________________________________________  
             </h1>
-            
+
+            <div className='flex flex-col'>
+  {/* Academic Year And Term */}
+  <div className='flex flex-row items-center'>
+    <label htmlFor="academicyearterm" className='ml-[100px] mt-[20px] font-serif w-[200px] text-right pr-[10px]'>Academic Year And Term</label>
+    <select id="academicyearterm" className='mt-[18px] h-[30px] w-[400px] bg-[#D1DDED] border-solid border-black border-[1px] rounded-[3px] font-sans'>
+    <option value="" selected>-- Select an option --</option>
+      <option value="2024-1st-Semester">2024 1st Semester</option>
+      <option value="2024-2nd-Semester">2024 2nd Semester</option>
+      <option value="2025-1st-Semester">2025 1st Semester</option>
+      <option value="2025-2nd-Semester">2025 2nd Semester</option>
+    </select>
+  </div>
+
+  {/* Academic Program */}
+  <div className='flex flex-row items-center'>
+    <label htmlFor="acadprog" className='ml-[100px] mt-[20px] font-serif w-[200px] text-right pr-[10px]'>Academic Program</label>
+    <select id="acadprog" className='mt-[18px] h-[30px] w-[400px] bg-[#D1DDED] border-solid border-black border-[1px] rounded-[3px] font-sans'>
+    <option value="" selected>-- Select an option --</option>
+      <option value="option1">Option 1</option>
+      <option value="option2">Option 2</option>
+      <option value="option3">Option 3</option>
+      <option value="option4">Option 4</option>
+    </select>
+  </div>
+
+  {/* Curriculum */}
+  <div className='flex flex-row items-center'>
+    <label htmlFor="curriculum" className='ml-[100px] mt-[20px] font-serif w-[200px] text-right pr-[10px]'>Curriculum</label>
+    <select id="curriculum" className='mt-[18px] h-[30px] w-[400px] bg-[#D1DDED] border-solid border-black border-[1px] rounded-[3px] font-sans'>
+    <option value="" selected>-- Select an option --</option>
+      <option value="option1">Option 1</option>
+      <option value="option2">Option 2</option>
+      <option value="option3">Option 3</option>
+      <option value="option4">Option 4</option>
+    </select>
+  </div>
+
+  {/* Current Year Level */}
+  <div className='flex flex-row items-center'>
+    <label htmlFor="curryrlvl" className='ml-[100px] mt-[20px] font-serif w-[200px] text-right pr-[10px]'>Current Year Level</label>
+    <select id="curryrlvl" className='mt-[18px] h-[30px] w-[400px] bg-[#D1DDED] border-solid border-black border-[1px] rounded-[3px] font-sans'>
+    <option value="" selected>-- Select an option --</option>
+      <option value="option1">Option 1</option>
+      <option value="option2">Option 2</option>
+      <option value="option3">Option 3</option>
+      <option value="option4">Option 4</option>
+    </select>
+  </div>
+
+  {/* Status */}
+  <div className='flex flex-row items-center'>
+    <label htmlFor="status" className='ml-[100px] mt-[20px] font-serif w-[200px] text-right pr-[10px]'>Status</label>
+    <select id="status" className='mt-[18px] h-[30px] w-[400px] bg-[#D1DDED] border-solid border-black border-[1px] rounded-[3px] font-sans'>
+    <option value="" selected>-- Select an option --</option>
+      <option value="option1">Option 1</option>
+      <option value="option2">Option 2</option>
+      <option value="option3">Option 3</option>
+      <option value="option4">Option 4</option>
+    </select>
+  </div>
+</div>  
+
 
             {/* CONTENT */}
           </div>
